c3f7dc08425856c41d2dff6fece57143d8c1748d
Gabriel Andrade Almeida <59920810+g-andradd@users.noreply.github.com>
2024-11-12 18:36:39 -0300

    fix: module to config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; // Importe o provideHttpClient
-
-import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
     provideClientHydration(),
-    CommonModule,
-    FormsModule
-  ]
+  ],
 };
